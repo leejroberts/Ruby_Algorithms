a021a13359f903c7a3809efdea4e662e34a15f73
leejroberts <leejamesroberts@gmail.com>
2018-08-15 08:56:49 -0400

    completed d_linked_list with passing specs

--- data_structures/d_linked_list.rb
+++ data_structures/d_linked_list.rb
@@ -1,72 +1,174 @@
 # frozen_string_literal: true
 
 class Node
-  attr_accessor :value, :next_node, :previous_node
-  def initialize(value = nil, next_node = nil, previous_node = nil)
+  attr_accessor :value, :previous_node, :next_node
+  def initialize(value = nil, previous_node = nil, next_node = nil)
     @value = value
-    @next_node = next_node
     @previous_node = previous_node
+    @next_node = next_node
   end
 end
 
 class DoubleLinkedList
+  attr_reader :head, :tail, :node_count
+
   def initialize(starting_value = nil)
     @head = Node.new(starting_value)
     @tail = @head
+    @node_count = 1
   end
 
-  def add(value)
-    if @head.value.nil?
-      @head.value = value
+  # returns the value at the index
+  def [](index)
+    node = node_at_index(index)
+    node.value
+  end
+
+  # sets the value at the index
+  def []=(index, value)
+    node = node_at_index(index)
+    return false unless node.is_a?(Node)
+    node_at_index(index).value = value
+  end
+
+  # adds the value to a Node, adds the node to the end of the list
+  def push(value)
+    @head.value.nil? ? @head.value = value : add_tail(value)
+    @node_count
+  end
+
+  # same as push
+  def <<(value)
+    push(value)
+  end
+
+  # returns the last value, removes the last node if @node_count > 1
+  def pop
+    if @node_count > 1
+      value = @tail.value
+      remove_tail
     else
-      new_node = Node.new(value, previous_node = @tail)
-      @tail.next_node = new_node
-      @tail = new_node
+      value = @head.value
+      @head.value = nil
     end
+    value
+  end
+
+  # adds value to the front of list
+  def shift(value)
+    @head.value ? add_head(value) : head.value = value
+  end
+
+  # returns the first value from the list, removes the node if @node_count > 1
+  def unshift
+    if @node_count > 1
+      value = @head.value
+      remove_head
+    else
+      value = @head.value
+      @head.value = nil
+    end
+    value
+  end
+
+  # will remove the first node containing the value
+  def remove(value)
+    node_to_remove = node_with_value(value)
+    return false unless node_to_remove
+    remove_node(node_to_remove)
+  end
+
+  def remove_node(node)
+    return false unless node.is_a?(Node) && @node_count > 1
+
+    if remove_node === @head
+      remove_head
+    elsif remove_node === @tail
+      remove_tail
+    else
+      before_node = node.next_node
+      after_node = node.next_node
+      join_nodes(before_node, after_node)
+    end
+    @node_count -= 1
+  end
+
+  def join_nodes(node_1, node_2)
+    node_1.next_node = node_2
+    node_2.previous_node = node_1
   end
 
   def length
-    node_count = 0
-    current_node = @head
-    node_count += 1 while current_node
-    node_count
+    @head.value ? @node_count : 0
   end
 
   def insert(value, index)
-    return false if index > length - 1
+    return false if out_of_range(index)
     after_node = node_at_index(index)
     if after_node.value.nil?
       after_node.value = value
-    elsif after_node.previous_node
+    else
       before_node = after_node.previous_node
-      insert_node = Node.new(value, after_node, before_node)
+      insert_node = Node.new(value, before_node, after_node)
       before_node.next_node = insert_node
       after_node.previous_node = insert_node
+      @node_count += 1
     end
+    @node_count
   end
 
   def include?(value)
     node_with_value(value) ? true : false
   end
 
+  private
+
+  # returns the first node with the given value
   def node_with_value(value)
     current_node = @head
     while current_node.value
       return current_node if current_node.value == value
       current_node = current_node.next_node
     end
-    false
   end
 
-  private
-
+  # returns the node at the given index
   def node_at_index(index)
-    list_index = 0
+    return nil if out_of_range(index)
     current_node = @head
-    until list_index == index
-      list_index += 1
+    index.times do
       current_node = current_node.next_node
     end
     current_node
+  end
+
+  def out_of_range(index)
+    index > node_count - 1 || index < 0
+  end
+
+  def remove_head
+    return false unless @node_count > 1
+    @head = @head.next_node
+    @node_count -= 1
+  end
+
+  def remove_tail
+    return false unless @node_count > 1
+    @tail = @tail.previous_node
+    @node_count -= 1
+  end
+
+  def add_head(value)
+    new_head = Node.new(value, next_node = @head)
+    @head.previous_node = new_head
+    @head = new_head
+    @node_count += 1
+  end
+
+  def add_tail(value)
+    new_tail = Node.new(value, previous_node = @tail)
+    @tail.next_node = new_tail
+    @tail = new_tail
+    @node_count += 1
   end
 end
--- spec/data_structures/d_linked_list_spec.rb
+++ spec/data_structures/d_linked_list_spec.rb
@@ -1,11 +1,66 @@
 # frozen_string_literal: true
 
-require_relative '../data_structures/d_linked_list'
-
+require_relative '../../data_structures/d_linked_list'
 RSpec.describe 'DoubleLinkedList' do
   let(:list) { DoubleLinkedList.new }
+  let(:list_w_value) { DoubleLinkedList.new(10) }
+  let(:list_zero_to_ten) { DoubleLinkedList.new }
+
+  before(:example) do
+    (0..10).each { |num| list_zero_to_ten << num }
+  end
 
   it 'can instantiate' do
     expect(list).to be_instance_of(DoubleLinkedList)
+  end
+
+  it 'can instantiate with a starting value' do
+    expect(list_w_value.head.value).to eq(10)
+    expect(list_w_value.tail.value).to eq(10)
+  end
+
+  it 'can push a value to an empty list' do
+    list << 'only value'
+    expect(list.head.value).to eq('only value')
+    expect(list.tail.value).to eq('only value')
+    expect(list.length).to eq(1)
+  end
+
+  it 'can push a value to the end' do
+    list_w_value.push('new_tail_value')
+    expect(list_w_value.tail.value).to eq('new_tail_value')
+    expect(list_w_value.length).to eq(2)
+  end
+
+  it 'can push values to the end using <<' do
+    list_w_value << 'new_new_tail'
+    expect(list_w_value.tail.value).to eq('new_new_tail')
+    expect(list_w_value.length).to eq(2)
+    list_w_value << 'final_tail'
+    expect(list_w_value.length).to eq(3)
+  end
+
+  it 'returns the correct length' do
+    expect(list_zero_to_ten.length).to eq(11)
+  end
+
+  it 'can have values added with push and return values with []' do
+    (0..10).each do |num_and_index|
+      expect(list_zero_to_ten[num_and_index]).to eq(num_and_index)
+    end
+  end
+
+  it 'can set values with []' do
+    list_zero_to_ten[5] = 'steve'
+    expect(list_zero_to_ten[5]).to eq('steve')
+  end
+
+  it 'will return false if the index is outside of range' do
+    expect(list.insert('Ally', 11)).to be(false)
+  end
+
+  it 'will set values at zero index if there is not one' do
+    list.insert('Ally', 0)
+    expect(list[0]).to eq('Ally')
   end
 end
