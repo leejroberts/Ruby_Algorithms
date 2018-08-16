eb5f6e7bf7f5c4c66aec2da846ce2d86435dff39
leejroberts <leejamesroberts@gmail.com>
2018-08-15 23:17:44 -0400

    continued work on bi_tree

--- data_structures/bi_tree.rb
+++ data_structures/bi_tree.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
-class Node
-  attr_accessor :value
+require 'pry'
 
-  def initialize(value)
+class BiNode
+  attr_accessor :value, :left_b, :right_b
+
+  def initialize(value = nil)
     @value = value
     @right_b = nil
     @left_b = nil
@@ -11,31 +13,82 @@ class Node
 end
 
 class BiTree
+  attr_accessor :root, :node_count
   def initialize(initial_value = nil)
-    @root = Node.new(initial_value)
-    @node_count = 0
+    @root = BiNode.new(initial_value)
+    @node_count = 1
   end
 
-  def add(value, current_node = @root, previous_node = nil)
-    if current_node && current_node.value.nil?
-      current_node.value = value
-    elsif current_node && current_node.value == value
-      nil
-    elsif current_node.nil?
-      new_node = Node.new(value)
-      if previous_node.value < value
-        previous_node.left_b = new_node
+  def has_no_values?
+    @root.value.nil?
+  end
+
+  def add(value, current_node: @root, previous_node: nil)
+    return @root.value = value if has_no_values?
+
+    if current_node.nil?
+      @node_count += 1
+      if value > previous_node.value
+        return previous_node.right_b = BiNode.new(value)
       else
-        previous_node.right_b = new_node
+        return previous_node.left_b = BiNode.new(value)
       end
-    elsif current_node.value > value
-      previous_node = current_node
-      current_node = current_node.left_b
-      add(value, current_node, previous_node)
+    elsif value == current_node.value
+      return nil
+    elsif value > current_node.value
+      return add(value, previous_node: current_node, current_node: current_node.right_b)
     else
-      previous_node = current_node
-      current_node = current_node.right_b
-      add(value, current_node, previous_node)
+      return add(value, previous_node: current_node, current_node: current_node.left_b)
     end
+  end
+
+  def find_node(value, current_node = @root)
+    if current_node.nil? || current_node.value.nil?
+      nil
+    elsif value == current_node.value
+      current_node
+    elsif value > current_node.value
+      find_node(value, current_node.right_b)
+    else
+      find_node(value, current_node.left_b)
+    end
+  end
+
+  def move_pointers(to_node, from_node)
+    to_node.value = from_node.value
+    to_node.right_b = from_node.right_b
+    to_node.left_b = from_node.left_b
+  end
+
+  def find_closest_node(current_node)
+    return current_node if current_node.left_b.nil?
+    find_closest_node(current_node.left_b)
+  end
+
+  def remove(value)
+    node = find_node(value)
+    return false if node.nil?
+
+    right_b = node.right_b
+    left_b = node.left_b
+    if right_b && left_b.nil?
+      move_pointers(node, right_b)
+    elsif left_b && right_b.nil?
+      move_pointers(node, left_b)
+    else
+      closest_node = find_closest_node(right_b)
+      node.value = closest_node.value
+      move_pointers(closest_node, closest_node.right_b) if closest_node.right_b
+    end
+    true
+  end
+
+  def count_nodes(current_node = @root, seen = [])
+    seen << current_node
+    node_left = current_node.right_b
+    node_right = current_node.left_b
+    count_nodes(node_left, seen) if node_left
+    count_nodes(node_right, seen) if node_right
+    seen.length
   end
 end
--- spec/data_structures/bi_tree_spec.rb
+++ spec/data_structures/bi_tree_spec.rb
@@ -1,10 +1,68 @@
 # frozen_string_literal: true
 
-require '../../data_structures/bi_tree'
+require_relative '../../data_structures/bi_tree'
 
-RSpec.xdescribe 'bi_tree' do
+RSpec.describe BiTree do
   let(:bi_tree) { BiTree.new }
+  let(:bi_tree_mult_values) { BiTree.new }
+  let(:bi_tree_lots_values) { BiTree.new }
+
+  before(:example) do
+    (1..10).to_a.shuffle.each { |num| bi_tree_mult_values.add(num) }
+    (1..100).to_a.shuffle.each { |num| bi_tree_lots_values.add(num) }
+  end
+
   it 'can initiialize' do
     expect(bi_tree).to be_instance_of(BiTree)
+  end
+
+  it 'can add a value' do
+    bi_tree.add(10)
+    bi_tree.add(20)
+    expect(bi_tree.root.value).to eq(10)
+    expect(bi_tree.node_count).to eq(2)
+  end
+
+  it 'can add multiple values' do
+    expect(bi_tree_mult_values.node_count).to eq(10)
+  end
+
+  it 'will not add duplicate values' do
+    bi_tree_mult_values.add(10)
+    expect(bi_tree_mult_values.node_count).to eq(10)
+  end
+
+  it 'returns nil if value not present' do
+    expect(bi_tree_mult_values.find_node(12)).to be_nil
+  end
+
+  it 'returns a node with the given value if value present' do
+    expect(bi_tree_mult_values.find_node(5).value).to eq(5)
+    expect(bi_tree_lots_values.find_node(22).value).to eq(22)
+  end
+
+  it 'returns a count for all nodes' do
+    expect(bi_tree_mult_values.count_nodes).to eq(10)
+  end
+
+  it 'can remove the value from a single node' do
+    bi_tree.add(20)
+    bi_tree.remove(20)
+    expect(bi_tree.find_node(20)).to be_falsy
+    expect(bi_tree.node_count).to eq(1)
+  end
+
+  it 'can remove a node and value' do
+    bi_tree.add(10)
+    bi_tree.add(20)
+    bi_tree.remove(10)
+    expect(bi_tree.find_node(5)).to be_falsy
+    expect(bi_tree.count_nodes).to eq(1)
+  end
+
+  it 'can remove a node and value' do
+    bi_tree_mult_values.remove(5)
+    expect(bi_tree_mult_values.find_node(5)).to be_nil
+    expect(bi_tree_mult_values.count_nodes).to eq(9)
   end
 end
