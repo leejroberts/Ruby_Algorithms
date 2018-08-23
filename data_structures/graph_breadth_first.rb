7c50e22ddf0c0a3c74d223d788c833a9a764672b
leejroberts <leejamesroberts@gmail.com>
2018-08-23 13:07:17 -0400

    basic start on breadth_first search within network of connected nodes

---
+++ data_structures/graph_breadth_first.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+class Person
+  attr_reader :name, :friends
+
+  def initialize(name)
+    @name = name
+    @friends = []
+  end
+
+  def add_friend(person_obj)
+    return nil if @friends.include?(person_obj)
+    @friends << person_obj
+  end
+end
+
+class Network
+  def initialize(first_member = nil)
+    @members = (first_member.instance_of(Person) ? [first_member] : [] )
+  end
+
+  def find_member_by_name(member_name)
+    @members.select { |member| if member.name == member_name }.first
+  end
+
+  def add_member(name, *friends)
+    if find_member_by_name(name).nil?
+      member = Person.new()
+
+  end
+
+end
